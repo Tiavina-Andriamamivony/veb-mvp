6dcaa5178021ddf7d6169ecaff898e476578099e
Tiavina-Andriamamivony <hei.tiavina.1@gmail.com>
2025-06-04 11:45:36 +0300

    feat(api): get all players endpoint.

---
+++ src/app/(backend)/api/route.ts
@@ -0,0 +1,7 @@
+import { NextResponse } from "next/server";
+import { prisma } from "../../../../lib/prisma";
+
+export async function GET(request: Request){
+    const players = await prisma.player.findMany();
+    return NextResponse.json(players);
+}
